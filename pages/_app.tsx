488c8d40c6f49b68c28ae71e7a32038e25cb9951
Richard <rcoric03@gmail.com>
2022-12-06 01:57:01 +0000

    SEO Compatibility!

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import '../styles/globals.css';
 import '../styles/Nav.scss';
+import { NextSeo } from 'next-seo';
 import type { AppProps } from 'next/app'
 import Nav from '../components/Navigation';
 import Footer from '../components/Footer';
@@ -7,6 +8,10 @@ import Footer from '../components/Footer';
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <NextSeo
+        title='Richard Coric | Web Developer'
+        description='Fullstack web developer aiming to build sites and help people'
+      />
       <Nav />
       <Component {...pageProps} />
       <Footer />
