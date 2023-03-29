61f6c263c55c2557abb84a3319d170554993bc54
Richard <rcoric03@gmail.com>
2023-03-29 02:55:48 +0100

    Add Store and Theme Base Code

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,8 +2,11 @@ import '../styles/globals.css';
 import '../styles/Nav.scss';
 import { NextSeo } from 'next-seo';
 import type { AppProps } from 'next/app';
+import { Provider } from 'react-redux';
 import Nav from '../components/Navigation';
 import Footer from '../components/Footer';
+import { store } from '../store';
+import Settings from '../components/Settings/Settings';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -12,9 +15,12 @@ export default function App({ Component, pageProps }: AppProps) {
         title="Richard Coric | Web Developer"
         description="Fullstack web developer aiming to build sites and help people"
       />
-      <Nav />
-      <Component {...pageProps} />
-      <Footer />
+      <Provider store={store}>
+        <Nav />
+        <Settings />
+        <Component {...pageProps} />
+        <Footer />
+      </Provider>
     </>
   );
 }
