9ceaa6b3db0db11b70b586754c8a5ae14332858e
Richard Coric <rcoric03@gmail.com>
2023-07-08 14:27:12 +0100

    im losing hope

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -13,7 +13,7 @@ class MyDocument extends Document {
       <Html lang="en" className="light">
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
           <link href="https://fonts.googleapis.com/css2?family=Alegreya:wght@400;700&family=Josefin+Slab:wght@700&family=Lora:ital,wght@0,400;0,700;1,400&display=swap" rel="stylesheet" />
         </Head>
         <body>
