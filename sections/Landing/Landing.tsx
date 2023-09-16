6f996f101bc67e0d475651222c15678b56ee4a14
Richard Coric <rcoric03@gmail.com>
2023-09-16 18:44:16 +0100

    Add animation

--- sections/Landing/Landing.tsx
+++ sections/Landing/Landing.tsx
@@ -2,12 +2,40 @@ import Pointer from '../../components/Pointer';
 import {
   StyledLandingSection, StyledHeading, StyledHeadingSecondary, StyledSpan,
 } from './Landing.styles';
+import Typewriter from 'typewriter-effect';
 
 function Landing() {
   return (
     <StyledLandingSection>
       <div>
-        <StyledHeading>Hi, I&apos;m Richard!</StyledHeading>
+        <StyledHeading>
+          <Typewriter
+            onInit={(typewriter) => {
+              typewriter
+              .typeString('Hi, I\'m Richard!')
+              .pauseFor(2500)
+              .deleteAll()
+              .typeString('Hallo, Ich bin Richard!')
+              .pauseFor(2500)
+              .deleteAll()
+              .typeString("Salut, Je suis Richard!")
+              .pauseFor(2500)
+              .deleteAll()
+              .typeString("Ahojte, Ja som Richard!")
+              .pauseFor(2500)
+              .deleteAll()
+              .typeString("안녕하세요, 리차드입니다~")
+              .pauseFor(2500)
+              .deleteAll()
+              .typeString('Hi, I\'m Richard!')
+              .start()
+              .callFunction(() => {
+                const cur = document.querySelector('.Typewriter__cursor') as Element; 
+                cur?.parentElement?.removeChild(cur);
+              });
+            }}
+            />
+          </StyledHeading>
         <StyledHeadingSecondary>
           A
           {' '}
